dbc73014b41bb29c60acbc93d3c133ecdd341408
Germán Contreras <234036903+germansango01@users.noreply.github.com>
2025-12-03 03:11:11 +0100

    refactor animation wall

--- Scenes/Player/ninja_frog.gd
+++ Scenes/Player/ninja_frog.gd
@@ -9,6 +9,8 @@ var max_jumps : int = 2
 var count_jumps : int = 0
 var double_jump : bool = false
 var ray_cast_dimention : float = 11.5
+var stuck_on_wall : bool = false
+var direction : float = 0
 
 func _ready() -> void:
 	$Animations.play("appearing")
@@ -19,10 +21,13 @@ func _physics_process(delta: float) -> void:
 		leaved_floor = false
 		had_jump = false
 		count_jumps = 0
+		if $CoyoteTimer:
+			$CoyoteTimer.stop()
 
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+		
 		# coyote timer init
 		if not leaved_floor:
 			$CoyoteTimer.start()
@@ -37,51 +42,66 @@ func _physics_process(delta: float) -> void:
 	
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	direction = Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 	
 	if $RayCastWall.get_collider():
-		$RayCastWall.get_collider().is_in_group("wall_jump")
-		print('toco zona amarilla')
+		if $RayCastWall.get_collider().is_in_group("wall_jump_group"):
+			if velocity.y > 0:
+				count_jumps = 0
+				velocity.y = 0
+				stuck_on_wall = true
+		else:
+			stuck_on_wall = false
+	else:
+		stuck_on_wall = false
+
 	
 	move_and_slide()
 	decide_animation()
 
 
 func decide_animation() -> void:
-	# wait for the first animation to finish
-	if not allow_animation: return
-	
-	# eje x
-	if velocity.x == 0:
-		# idle animation
-		$Animations.play("idle")
-	elif velocity.x < 0:
-		# walk left
+	# validate ray cast wall
+	if direction < 0:
 		$Animations.flip_h = true
 		$RayCastWall.target_position.x = -ray_cast_dimention
-		$Animations.play("run")
-	elif velocity.x > 0:
-		# walk right
+	elif direction > 0:
 		$Animations.flip_h = false
 		$RayCastWall.target_position.x = ray_cast_dimention
-		$Animations.play("run")
+
+	# wait for the first animation to finish
+	if not allow_animation: return
+
+	if stuck_on_wall:
+		$Animations.play("stuck_on_wall")
+	else:
+		# eje x
+		if velocity.x == 0:
+			# idle animation
+			$Animations.play("idle")
+		elif velocity.x < 0:
+			# walk left
+			$Animations.play("run")
+		elif velocity.x > 0:
+			# walk right
+			$Animations.play("run")
 		
-	# eje y
-	if velocity.y < 0:
-		# jump animation
-		$Animations.play("jump")
-	elif velocity.y > 0:
-		# fall animation
-		$Animations.play("fall")
-	# double jump animation
-	if double_jump:
-		double_jump = false
-		allow_animation = false
-		$Animations.play("double_jump")
+		# eje y
+		if velocity.y < 0:
+			# jump animation
+			$Animations.play("jump")
+		elif velocity.y > 0:
+			# fall animation
+			$Animations.play("fall")
+		# double jump animation
+		if double_jump:
+			double_jump = false
+			allow_animation = false
+			$Animations.play("double_jump")
 
 # jump
 func right_to_jump():
@@ -90,7 +110,7 @@ func right_to_jump():
 			return true
 		else: 
 			return false
-	if is_on_floor(): 
+	if is_on_floor() || stuck_on_wall: 
 		had_jump = true
 		return true
 	elif not $CoyoteTimer.is_stopped(): 
